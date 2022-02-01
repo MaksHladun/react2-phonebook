f190816d43832403aa63da13cfe458206611f4d1
Maks Hladun <makshladun@gmail.com>
2022-02-01 19:49:40 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 class App extends Component {
    state = {
     contacts: [],
-    filter: '',
+    filter: ' ',
    }
    componentDidMount(){
      const contacts = localStorage.getItem('contacts');
